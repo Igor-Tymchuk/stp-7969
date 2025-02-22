363ba2a65dc779e1306dc1a355d7c664596cf67b
Igor Tymchuk <dinamit.shuher@gmail.com>
2025-02-22 14:10:09 +0200

    fix swiper/ios sections/ comment cookie logs

--- src/js/cookie-modal.js
+++ src/js/cookie-modal.js
@@ -2,28 +2,28 @@ const modal = document.getElementById('cookie-modal');
 const acceptBtn = document.getElementById('accept-cookies');
 const declineBtn = document.getElementById('decline-cookies');
 
-console.log('Script is running');
+// console.log('Script is running');
 
 if (typeof Storage !== 'undefined') {
-  console.log('localStorage is available.');
+  // console.log('localStorage is available.');
 } else {
-  console.log('localStorage is NOT available.');
+  // console.log('localStorage is NOT available.');
 }
 
 if (!localStorage.getItem('cookiesChoice')) {
-  console.log('Cookies not accepted yet. Showing banner after 2 seconds.');
+  // console.log('Cookies not accepted yet. Showing banner after 2 seconds.');
 
   setTimeout(() => {
     modal.style.display = 'flex';
   }, 2000);
 } else {
-  console.log('Cookies already accepted or declined.');
+  // console.log('Cookies already accepted or declined.');
 }
 
 function hideBanner() {
-  console.log('Hiding banner and saving choice.');
+  // console.log('Hiding banner and saving choice.');
   localStorage.setItem('cookiesChoice', 'true');
-  console.log('CookiesChoice saved:', localStorage.getItem('cookiesChoice'));
+  // console.log('CookiesChoice saved:', localStorage.getItem('cookiesChoice'));
 
   modal.style.display = 'none';
 }
--- src/js/swiper-gallery.js
+++ src/js/swiper-gallery.js
@@ -1,23 +1,29 @@
 import Swiper from 'swiper';
 import { Keyboard, Mousewheel, Autoplay } from 'swiper/modules';
 import 'swiper/css';
-import 'swiper/css/pagination';
 
-const swiper = new Swiper('.swiper', {
-  modules: [Keyboard, Mousewheel, Autoplay],
-  effect: 'slide',
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 'auto',
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: true,
-  },
-  keyboard: {
-    enabled: true,
-  },
-  mousewheel: {
-    thresholdDelta: 70,
-  },
-  loop: true,
-});
+window.onload = () => {
+  const swiper = new Swiper('.swiper', {
+    modules: [Keyboard, Mousewheel, Autoplay],
+    effect: 'slide',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    slidesPerGroup: 1,
+    loop: true,
+    // initialSlide: 1,
+    // loopedSlides: 4,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: true,
+    },
+    keyboard: {
+      enabled: true,
+    },
+    mousewheel: {
+      thresholdDelta: 70,
+    },
+    // observer: true,
+    // observeParents: true,
+  });
+};
